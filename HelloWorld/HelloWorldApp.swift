47a5cd3b115a0c69d5ab9d026eda67346d2d6516
tehya-laughlin <tehyanlaugh.shop@gmail.com>
2023-08-28 21:50:18 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Tehya Laughlin on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
